9991b689ed663bb7e54b35ba12c32a26ee53bb72
Isabella Smallcombe <isabella.smallcombe@gmail.com>
2021-06-11 01:27:32 +1000

    getting usernames done, need to get IPFs working

--- src/CreatorCard.js
+++ src/CreatorCard.js
@@ -1,10 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './CreatorCard.css';
 
 const CreatorCard = ({ id, netRevenue, nfts}) => {
 
     const [open, setOpen] = useState(false);
     const [nftMetadata, setNftMetadata] = useState([]);
+    const [username, setUsername] = useState('');
+    const [profilePicture, setProfilePicture] = useState('');
+    const [ipfsPath, setIPFSPath] = useState('');
+
+    useEffect(() => {
+            fetch('https://stark-mountain-23648.herokuapp.com/https://api.foundation.app/graphql', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Request-Method': 'POST',
+                'Access-Control-Request-Headers': 'Content-Type, Authorization',
+                'Access-Control-Allow-Origin':  '*',
+            },
+            body: JSON.stringify({
+                query: `
+                    {
+                        user(publicKey: "${id}") {
+                            username
+                            profileImageUrl
+                        }
+                    }
+                `
+            })
+        })
+        .then((res) => res.json())
+        .then(res => {
+            setUsername(res.data.user.username);
+            setProfilePicture(res.data.user.profileImageUrl);
+        }).catch(error => console.log(error));
+    });
 
     // const nfts = '';
     // example ID: "0xf74d1224931afa9cf12d06092c1eb1818d1e255c-24437"
@@ -15,17 +45,27 @@ const CreatorCard = ({ id, netRevenue, nfts}) => {
 
                 const tokenId = nfts[i].id.split('-');
                 console.log(tokenId);
-                console.log(`https://api.opensea.io/api/v1/asset/${id}/${tokenId[1]}/`);
-                fetch(`https://api.opensea.io/api/v1/asset/${id}/${nfts[i]}/`, {
-                method: 'GET', 
+                fetch('https://api.thegraph.com/subgraphs/name/f8n/fnd', {
+                    method: 'POST', 
                     headers: {
-                    'Content-Type': 'application/json',
+                        'Content-Type': 'application/json',
                     },
-                })
-                .then((res) => res.json())
-                .then(res => {
-                    console.log(res);
-                }).catch(error => console.log(error));
+                    body: JSON.stringify({
+                        query: `
+                            {
+                                nfts(where: {tokenId: ${tokenId[1]}}) {
+                                    tokenIPFSPath
+                                }
+                            }
+                        `
+                        })
+                    })
+                    .then((res) => res.json())
+                    .then(res => {
+                        console.log(res.data.nfts[0].tokenIPFSPath);
+                        const ipfsString = "" + res.data.nfts[0].tokenIPFSPath;
+                        setIPFSPath(res.data.nfts[0].tokenIPFSPath)
+                    }).catch(error => console.log(error));
         }
     };
     
@@ -35,19 +75,27 @@ const CreatorCard = ({ id, netRevenue, nfts}) => {
             setOpen(false);
         }else{
             setOpen(true);
-            handleNftMetadata();
+            //handleNftMetadata();
         }
     };
 
     return (
         <div className='creator-card'>
-            <text>{id}</text>
+            <text>@{username}</text>
+            <img className='profile-picture' src={profilePicture}/>
             <text>{netRevenue} ETH</text>
             <text>NFTs Created {nfts.length}</text>
-            <div className="nft-embed"></div>
             <button onClick={onClickHandler}>Drop down</button>
             {open && (
+                <>
                 <text> hello</text>
+                <div className='nft-row'>
+                    <img className='nft-display' src="https://ipfs.io/ipfs/QmauUZUuQf4z9AT21jMGKKuvuRi1FDEA1ev3BevQETSp83/nft.png" />
+                    <img className='nft-display' src="https://ipfs.io/ipfs/QmauUZUuQf4z9AT21jMGKKuvuRi1FDEA1ev3BevQETSp83/nft.png" />
+                    <img className='nft-display' src="https://ipfs.io/ipfs/QmauUZUuQf4z9AT21jMGKKuvuRi1FDEA1ev3BevQETSp83/nft.png" />
+                </div>
+                <a className="button-find-more" href={`https://foundation.app/${username}`} target="_blank">View more on Foundation</a> 
+                </>
             )}
         </div>
     )
