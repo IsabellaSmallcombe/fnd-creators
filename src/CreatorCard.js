589169a76c712e5d1130c15594c61f46b2e326ed
Isabella Smallcombe <isabella.smallcombe@gmail.com>
2021-06-27 23:07:54 +1000

    Working on nft modal

--- src/CreatorCard.js
+++ src/CreatorCard.js
@@ -8,14 +8,16 @@ const CreatorCard = ({ id, netRevenue, nfts}) => {
     // const [open, setOpen] = useState(false);
     const [username, setUsername] = useState('');
     const [profilePicture, setProfilePicture] = useState('');
-    // const [nftMetadata, setNftMetadata] = useState([]);
+    const [nftMetadata, setNftMetadata] = useState([]);
     // const [ipfsPath, setIPFSPath] = useState('');
+    //const nftIPFS = [];
+    const [allMetadataFetched, setAllMetadataFetched] = useState(false);
 
     const revenue = Math.round(parseInt(netRevenue) * 10) / 10;
 
     useEffect(() => {
             fetch('https://stark-mountain-23648.herokuapp.com/https://api.foundation.app/graphql', {
-            method: 'POST', 
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Request-Method': 'POST',
@@ -44,49 +46,56 @@ const CreatorCard = ({ id, netRevenue, nfts}) => {
 
     const openModal = () => {
         setIsOpen(true);
+        handleNftMetadata();
     }
     
     const closeModal = () => {
         setIsOpen(false);
     }
 
-    // const handleNftMetadata = () => {
-    //     for (var i = 0; i < nfts.length && i < 3; i++) {
-    //         console.log('hi');
-    //         const tokenId = nfts[i].id.split('-');
-    //         console.log(tokenId);
-    //         fetch('https://api.thegraph.com/subgraphs/name/f8n/fnd', {
-    //             method: 'POST', 
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //             },
-    //             body: JSON.stringify({
-    //                 query: `
-    //                     {
-    //                         nfts(where: {tokenId: ${tokenId[1]}}) {
-    //                             tokenIPFSPath
-    //                         }
-    //                     }
-    //                 `
-    //                 })
-    //             })
-    //             .then((res) => res.json())
-    //             .then(res => {
-    //                 console.log(res.data.nfts);
-    //                 const ipfsString = "" + res.data.nfts[0].tokenIPFSPath;
-    //                 setIPFSPath(res.data.nfts[0].tokenIPFSPath)
-    //             }).catch(error => console.log(error));
-    //     }
-    // };
+    const handleNftMetadata = () => {
+        for (var i = 0; i < nfts.length && i < 3; i++) {
+            console.log('hi');
+            const tokenId = nfts[i].id.split('-');
+            console.log(tokenId);
+            fetch('https://api.thegraph.com/subgraphs/name/f8n/fnd', {
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    query: `
+                        {
+                            nfts(where: {tokenId: ${tokenId[1]}}) {
+                                tokenIPFSPath
+                            }
+                        }
+                    `
+                    })
+                })
+                .then((res) => res.json())
+                .then(res => {
+                    console.log('***** ', res.data.nfts);
+                    //const ipfsString = "" + res.data.nfts[0].tokenIPFSPath;
+                    nftJsonFetch(res.data.nfts[0].tokenIPFSPath);
+                    // setIPFSPath(res.data.nfts[0].tokenIPFSPath)
+                }).catch(error => console.log(error));
+        }
+        setAllMetadataFetched(true);
+    };
 
-    // const onClickHandler = () => {
-    //     if (open) {
-    //         setOpen(false);
-    //     }else{
-    //         setOpen(true);
-    //         //handleNftMetadata();
-    //     }
-    // };
+    const nftJsonFetch = (jsonPath) => {
+        fetch(`https://ipfs.io/ipfs/${jsonPath}`)
+            .then((res) => res.json())
+            .then(res => {
+                setNftMetadata(res.image);
+            }).catch(error => console.log(error));
+    }
+
+    const nft_display = {
+        'max-width': '50px',
+        margin: '10px'
+    };
 
     return (
         <div className='creator-card'>
@@ -100,11 +109,12 @@ const CreatorCard = ({ id, netRevenue, nfts}) => {
                 </a>
                 <button onClick={openModal} className="text-details">NFTs Created {nfts.length}</button>
                 <Modal isOpen={isOpen} onRequestClose={closeModal}>
-                    {nfts.map((nft, index) => {
-                        const token = nft.id.split('-');
+                    {nftMetadata.map((nft, index) => {
                         return (
-                            <div className='nft-modal'>
-                                <NFTE className="nft" key={index} contract={token[0]} tokenId={token[1]}/>
+                            <div>
+                                <div className='nft-flex'>
+                                    <image key={index} src={nft} />
+                                </div>
                             </div>
                         )
                     })}
